b3055a97df9d2d20ebf00b89073b43ac9c97a208
felipealvescosta <felipealves00007@gmail.com>
2020-08-05 00:07:06 -0300

    chore: project pages

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../src/styles/global.css';
+import '../src/pages/Landing';
+import Landing from '../src/pages/Landing';
+function App() {
+  return (
+    <Landing/>
+  );
+}
+
+export default App;
