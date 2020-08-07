c87e4f145e1f5d6a184147aa3ec091e9a5622d39
felipealvescosta <felipealves00007@gmail.com>
2020-08-07 18:46:15 -0300

    chore: add backend files

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,8 +5,30 @@ import PageHeader from '../../components/PageHeader'
 function TeacherForm() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Incrível que você quer dar aulas!"/>
+      <PageHeader title="Incrível que você quer dar aulas!"
+        description="Faca sua inscrição"
+      />
     </div>
+    <main>
+      <fieldset>
+        <legend>Seus dados</legend>
+
+        <div className="input-block">
+            <label htmlFor="name">Nome Completo</label>
+            <input type="text" id="name"/>
+        </div>
+
+        <div className="input-block">
+            <label htmlFor="avatar">Nome Completo</label>
+            <input type="text" id="avatar"/>
+        </div>
+
+        <div className="input-block">
+            <label htmlFor="whatsapp">WhatsApp</label>
+            <input type="text" id="whatsapp"/>
+        </div>
+      </fieldset>
+    </main>
   );
 }
 
